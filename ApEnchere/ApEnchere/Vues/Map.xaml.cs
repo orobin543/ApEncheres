3c72c032a97249fbd115205887f4eb9bfbfc25c8
maeva22 <43241212+maeva22@users.noreply.github.com>
2022-05-09 18:17:02 +0200

    réparation code

--- ApEnchere/ApEnchere/Vues/Map.xaml.cs
+++ ApEnchere/ApEnchere/Vues/Map.xaml.cs
@@ -1,13 +1,9 @@
-﻿using ApEnchere.VueModeles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
-using Xamarin.Forms;
-using Xamarin.Forms.Maps;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
+using ApEnchere.VueModeles;
+using System;
 
 namespace ApEnchere.Vues
 {
